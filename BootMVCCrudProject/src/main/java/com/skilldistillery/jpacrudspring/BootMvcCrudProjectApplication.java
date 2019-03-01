b7d6a1cc9ea0be0dfaf13fcc26dceee669dc228f
Todd Trowbridge <toddtrowbridge@Brucebs-MacBook-Pro.local>
2019-03-01 15:45:07 -0700

    added/renamed packages

---
+++ BootMVCCrudProject/src/main/java/com/skilldistillery/jpacrudspring/BootMvcCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacrudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcCrudProjectApplication.class, args);
+	}
+
+}
---
+++ BootMVCCrudProject/src/test/java/com/skilldistillery/jpacrudspring/BootMvcCrudProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.jpacrudspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootMvcCrudProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
